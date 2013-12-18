06f083e35cf17b81015009c9bc8a21e9ba718910
Timothy Chang <hk.timchang@gmail.com>
2013-12-18 21:16:56 +0800

    Started experimenting with navigation bar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,6 +6,30 @@
     <link href="/cslog/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
+    
+    <nav class="navbar navbar-default" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Ambi Cstrike</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Players</a></li>
+          <li><a href="#">Matches</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </nav>
     <div class="col-md-2"></div>
     <div class="col-md-8">
     <table class="table">
