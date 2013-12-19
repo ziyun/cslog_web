9c4c6531fdee9ac7981d7541e079577753151d0a
Timothy Chang <tim@echogroupholdings.com>
2013-12-19 11:24:51 +0800

    Removed 'Players' from navbar for now

--- application/views/header.php
+++ application/views/header.php
@@ -22,7 +22,6 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li><a href="#">Players</a></li>
           <li><a href="/cslog/match">Matches</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
